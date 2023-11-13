85852a6c90d2d4dd2f5bd0a571319f153e44b0c2
hsoaresbianca <biasoares.bianca@gmail.com>
2023-11-13 17:18:50 -0300

    desafio resolvido utilizando classe/função/objeto

--- indexClasse.js
+++ indexClasse.js
@@ -1,10 +1,34 @@
 //RESOLVENDO O PROJETO UTILIZANDO CLASSES, OBJETOS E FUNÇÕES:
 
-class dadosIniciais {
-    constructor(nome, idade, nomeHeroiTipoAtaque){
+class dadosHeroi {
+    constructor(nome, idade, heroiAtaque) {
         this.nome = nome;
         this.idade = idade;
-        this.heroi = heroi;
-        this.ataque = ataque;
+        this.heroiAtaque = heroiAtaque;
+    }
+
+    atacar() {
+        for (let index in this.heroiAtaque) {
+            let [tipo, ataque] = this.heroiAtaque[index];
+            console.log(`O ${tipo} atacou usando ${ataque}`);
+        }
     }
 }
+
+let dados = {
+    nome: "Bia",
+    idade: 25,
+    heroiAtaque: {
+        0: ["guerreiro", "espada"],
+        1: ["mago", "magia"],
+        2: ["monge", "artes marciais"],
+        3: ["ninja", "shuriken"],
+    },
+};
+
+function generateInvoice(dados) {
+    let heroi = new dadosHeroi(dados.nome, dados.idade, dados.heroiAtaque);
+    heroi.atacar();
+}
+
+generateInvoice(dados);
