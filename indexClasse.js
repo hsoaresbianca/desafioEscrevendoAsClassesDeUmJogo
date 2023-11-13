2e44d8516aaddfb3c5a688afa3e72f6351436915
hsoaresbianca <biasoares.bianca@gmail.com>
2023-11-13 12:28:10 -0300

    novo arquivo para resolver desafio de outra forma

---
+++ indexClasse.js
@@ -0,0 +1,10 @@
+//RESOLVENDO O PROJETO UTILIZANDO CLASSES, OBJETOS E FUNÇÕES:
+
+class dadosIniciais {
+    constructor(nome, idade, nomeHeroiTipoAtaque){
+        this.nome = nome;
+        this.idade = idade;
+        this.heroi = heroi;
+        this.ataque = ataque;
+    }
+}
